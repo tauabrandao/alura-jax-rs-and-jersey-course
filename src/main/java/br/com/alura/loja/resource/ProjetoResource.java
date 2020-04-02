e11813990e59c534e6c12b3290583fcd87d29793
Tauã Brandão <brandao.taua@gmail.com>
2020-04-01 23:04:17 -0300

    recursos de post e testes

--- src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,10 +1,13 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
@@ -14,9 +17,19 @@ public class ProjetoResource {
 
 	@Path("{id}")
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_XML)
 	public String busca(@PathParam("id") long id) {
 		Projeto projeto = new ProjetoDAO().busca(id);
-		return projeto.toJson();
+		return projeto.toXML();
+	}
+
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) {
+
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		new ProjetoDAO().adiciona(projeto);
+		return "<status>sucesso</status>";
+
 	}
 }
--- src/test/java/br/com/alura/loja/CienteTest.java
+++ src/test/java/br/com/alura/loja/CienteTest.java
@@ -2,7 +2,10 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -13,6 +16,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 
 public class CienteTest {
@@ -48,6 +52,31 @@ public class CienteTest {
 		String conteudo = target.path("/projetos/1").request().get(String.class);
 		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 		Assert.assertEquals("Minha loja", projeto.getNome());
+	}
+
+	@Test
+	public void testaQueSuportaNovosCarrinhos() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314l, "Tablet", 999, 1));
+		carrinho.setRua("Rua Teste de Desenvolvimento");
+		carrinho.setCidade("Florianopolis");
+		String xml = carrinho.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
+	}
+
+	@Test
+	public void testaQueSuportaNovosProjetos() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		Projeto projeto = new Projeto("Projeto teste", 343l, 2019);
+		String xml = projeto.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		Response response = target.path("/projetos").request().post(entity);
+		Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 
 }
